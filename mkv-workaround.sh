adf4d56daff0f79f567bfe7ec18f3a3ee1263d65
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2021-10-24 22:11:20 -0400

    Update mkv-workaround.sh

--- mkv-workaround.sh
+++ mkv-workaround.sh
@@ -4,10 +4,7 @@ do
   filename=$(basename "$file")
   extension="${filename##*.}"
   video_file="${filename%.*}"
-  output_video_file="${filename%.*}"
-  #fqfn = realpath $file
-  #output = "${fqfn%.*}"
-  #output=$video_file + ".mp4"
-  echo $output_video_file
+  #output_video_file="${filename%.*}"
+  #echo $output_video_file
   #ffmpeg -i $fqfn -c:a aac -c:v copy output.m4a "$(fqfn%.*}".mp4;
 done
