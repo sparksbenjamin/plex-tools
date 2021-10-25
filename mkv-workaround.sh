71216fb60dfcedad3db8c52ed8783b8ff15c8892
BENJAMIN SPARKS <sparksbenjamin@gmail.com>
2021-10-24 22:01:14 -0400

    Update mkv-workaround.sh

--- mkv-workaround.sh
+++ mkv-workaround.sh
@@ -4,6 +4,8 @@ do
   filename=$(basename "$file")
   extension="${filename##*.}"
   video_file="${filename%.*}"
+  
   fqfn = realpath $file
-  ffmpeg -i $fqfn -c:a aac -c:v copy output.m4a $(fqfn%.*}.mp4";
+  
+  ffmpeg -i $fqfn -c:a aac -c:v copy output.m4a "$(fqfn%.*}".mp4;
 done
